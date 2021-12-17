4547ee71f9a62a34d227c40c93c247673bfd1a92
Talent IT World <88759926+talentitworldadmin@users.noreply.github.com>
2021-12-17 09:15:36 +0530

    Delete ec2.tf

--- ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "demo-instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = aws_key_pair.demokeypartf.key_name
-  vpc_security_group_ids = var.sg_id
-
-  tags = local.common_tags
-}
